35a660dcfccbecd656e5bcccf4ff826afef6ed36
오루빠루 <ks00141@naver.com>
2022-05-10 16:13:03 +0900

    초기 커밋.

---
+++ ThicknessDataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ThicknessDataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ThicknessDataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7850f1bb-e3d3-4dad-80d3-29bfab57e3f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ThicknessDataTest/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ThicknessDataTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CSV파일읽기()
+        {
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+            Console.WriteLine(reader.ReadLine());
+            Console.WriteLine(reader.ReadLine());
+            Console.WriteLine(reader.ReadLine());
+
+        }
+
+        [TestMethod]
+        public void 헤더_날리기()
+        {
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+            reader.ReadLine();
+            reader.ReadLine();
+            Console.WriteLine(reader.ReadLine());
+        }
+
+        [TestMethod]
+        public void csv_행수()
+        {
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+            int rowCount = 0;
+            while(reader.ReadLine() != null)
+            {
+                rowCount++;
+            }
+            Console.WriteLine(rowCount);
+        }
+
+        [TestMethod]
+        public void 측정포인트_개수()
+        {
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+
+            var columns = reader.ReadLine().Split(',');
+            int sitesColumnIndex = Array.IndexOf(columns, "SITES");
+            reader.ReadLine();
+            var readLine = reader.ReadLine();
+            var data = readLine.Split(',');
+            Console.WriteLine(data[sitesColumnIndex]);
+            
+        }
+
+        [TestMethod]
+        public void 측정_매수()
+        {
+            int rowCount = 0;
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+            reader.ReadLine();
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                rowCount++;
+                reader.ReadLine();
+            }
+            Console.WriteLine(rowCount/2);
+
+        }
+
+        [TestMethod]
+        public void 측정웨이퍼_리스트출력()
+        {
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+            var columns = reader.ReadLine().Split(',');
+            int waferIdColumnIndex = Array.IndexOf(columns, "WAFER ID");
+            reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                Console.WriteLine(reader.ReadLine().Split(',')[waferIdColumnIndex]);
+                reader.ReadLine();
+            }
+
+        }
+
+        [TestMethod]
+        public void 측정로트_리스트출력()
+        {
+            var reader = new StreamReader(File.OpenRead(@"2022_05_10.csv"));
+            var columns = reader.ReadLine().Split(',');
+            int lotIdColumnIndex = Array.IndexOf(columns, "LOT ID");
+            reader.ReadLine();
+
+            List<string> array = new List<string>();
+            Dictionary<string, bool> lotDict = new Dictionary<string, bool>();
+            while (!reader.EndOfStream)
+            {
+                string lotId = reader.ReadLine().Split(',')[lotIdColumnIndex];
+                if (!lotDict.ContainsKey(lotId))
+                {
+                    lotDict[lotId] = true;
+                    array.Add(lotId);
+                }
+            }
+            foreach(string str in array)
+            {
+                Console.WriteLine(str);
+            }
+            /*int lotCount = lotDict.Keys.Count;
+            string[] lotList = new string[lotCount];
+            lotDict.Keys.CopyTo(lotList,0);
+            Console.WriteLine(lotList[0]);*/
+
+
+        }
+    }
+}
